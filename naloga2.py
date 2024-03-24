ab0f6f6af7e369b3ac53c9ccbdb850d4646b117a
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-24 19:34:54 +0100

    Filtriranje z Gaussovim jedrom done

--- naloga2.py
+++ naloga2.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2 as cv
 import numpy as np
 
@@ -17,17 +19,23 @@ def konvolucija(slika, jedro):
             output[i, j] = np.sum(calc[i:(vispad * 2 + 1 + i), j:(sirpad * 2 + 1 + j)] * jedro)
 
     return output
-    pass
+
 
 def filtriraj_z_gaussovim_jedrom(slika, sigma):
     '''Filtrira sliko z Gaussovim jedrom..'''
-
-    pass
+    velikost_jedra = int(2 * sigma * 2 + 1)
+    k = np.float32(velikost_jedra / 2 - 1 / 2)
+    jedro = np.zeros((velikost_jedra, velikost_jedra))
+    for i in range(velikost_jedra):
+        for j in range(velikost_jedra):
+            a = 1 / (2 * np.pi * sigma ** 2)
+            b = -(((i-k)**2 + (j-k)**2)/(2*sigma**2))
+            jedro[i, j] = a * np.exp(b)
+    return konvolucija(slika, jedro)
 
 
 def filtriraj_sobel_smer(slika):
     '''Filtrira sliko z Sobelovim jedrom in označi gradiente v orignalni sliki glede na ustrezen pogoj.'''
-
     pass
 
 
