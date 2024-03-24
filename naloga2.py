Updated finished exercise and added the report. Fixed color gradient bug. Added named window so image wouldn't move on change.

naloga2.py
@@ -29,7 +29,7 @@ def filtriraj_z_gaussovim_jedrom(slika, sigma):
     for i in range(velikost_jedra):
         for j in range(velikost_jedra):
             a = 1 / (2 * np.pi * sigma ** 2)
-            b = -(((i-k)**2 + (j-k)**2)/(2*sigma**2))
+            b = -(((i - k) ** 2 + (j - k) ** 2) / (2 * sigma ** 2))
             jedro[i, j] = a * np.exp(b)
     return konvolucija(slika, jedro)
 
@@ -44,17 +44,38 @@ def filtriraj_sobel_smer(slika):
 
 
 if __name__ == '__main__':
-    slika = cv.imread(".utils/lenna.png")
-    slika = cv.cvtColor(slika, cv.COLOR_BGR2GRAY)
-    # slika = cv.resize(slika, (1000, 1000))
-    slika = filtriraj_sobel_smer(slika)
-    show = cv.cvtColor(slika, cv.COLOR_GRAY2BGR)
-    vis, sir = slika.shape
-    for i in range(vis):
-        for j in range(sir):
-            if slika[i, j] > 120:
-                show[i, j] = (255, slika[i, j] / 2, slika[i, j] / 2)
-    cv.imshow("Filtered", show)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    pics = ["Sources/still_image.jpg", ".utils/lenna.png"]
+    cv.namedWindow("pic_window")
+    key = ord('a')
+    for item in pics:
+        cv.setWindowTitle("pic_window", "Input image")
+        for do in range(4):
+            slika = cv.imread(item)
+            if do == 1:
+                cv.setWindowTitle("pic_window", "Normal light")
+            elif do == 2:
+                cv.setWindowTitle("pic_window", "Bright light")
+                slika = cv.convertScaleAbs(slika, beta=100)
+            elif do == 3:
+                cv.setWindowTitle("pic_window", "Dark light")
+                slika = cv.convertScaleAbs(slika, beta=-100)
+            slika = cv.resize(slika, (500, 500))
+            show = slika
+            if do > 0:
+                slika = cv.cvtColor(slika, cv.COLOR_BGR2GRAY)
+                slika = filtriraj_sobel_smer(slika)
+                show = cv.cvtColor(slika, cv.COLOR_GRAY2BGR)
+                vis, sir = slika.shape
+                for i in range(vis):
+                    for j in range(sir):
+                        if slika[i, j] > 120:
+                            show[i, j] = (255, 0, 0)
+                            # show[i, j] = (255, slika[i, j] / 2, slika[i, j] / 2)
+            cv.imshow("pic_window", show)
+            key = cv.waitKey(0)
+            cv.destroyAllWindows()
+            if key == ord('q') or key == ord('n'):
+                break
+        if key == ord('q'):
+            break
     pass