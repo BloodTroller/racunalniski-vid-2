bcdcfe29a71822745f7247b6d677fb91a2adab55
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-24 18:41:09 +0100

    Konvolucija done

--- naloga2.py
+++ naloga2.py
@@ -5,6 +5,18 @@ import numpy as np
 def konvolucija(slika, jedro):
     '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
     Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
+    output = slika.copy()
+    vis, sir = slika.shape
+    vis_j, sir_j = jedro.shape
+    vispad = vis_j // 2
+    sirpad = sir_j // 2
+    calc = np.pad(slika, ((vispad, vispad), (sirpad, sirpad)), 'reflect')
+
+    for i in range(vis):
+        for j in range(sir):
+            output[i, j] = np.sum(calc[i:(vispad * 2 + 1 + i), j:(sirpad * 2 + 1 + j)] * jedro)
+
+    return output
     pass
 
 def filtriraj_z_gaussovim_jedrom(slika, sigma):
